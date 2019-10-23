3c4e5054353261a07bb92b58177e634d3b3867fe
Sina Golestanirad <sina.golestanirad@farmersedge.ca>
2019-10-23 15:10:16 -0600

    removed logger middleware from PROD

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import { persistReducer, persistStore } from "redux-persist";
-import storage from 'redux-persist/lib/storage';
+import storage from "redux-persist/lib/storage";
 /// project files
 import rootReducer from "./root-reducer";
 
@@ -10,8 +10,13 @@ const persistConfig = {
   storage,
   whitelist: ["cart"]
 };
+const middleware = [];
+
 const PReducer = persistReducer(persistConfig, rootReducer);
 const logger = createLogger({});
-const store = createStore(PReducer, applyMiddleware(logger));
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+const store = createStore(PReducer, applyMiddleware(...middleware));
 const persistor = persistStore(store);
-export  { store, persistor };
+export { store, persistor };
